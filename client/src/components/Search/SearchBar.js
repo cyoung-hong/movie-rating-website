a5f0f55506a25b0d866d7dcefc28c50915053f21
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-03-28 18:16:18 -0400

    added handleclick event for searching

--- client/src/components/Search/SearchBar.js
+++ client/src/components/Search/SearchBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Paper,
@@ -8,15 +8,13 @@ import {
   Typography,
 } from "@material-ui/core";
 
+import { Link } from "react-router-dom";
+
 import SearchIcon from "@material-ui/icons/Search";
 
-import axios from 'axios';
+import { searchMovieByTitle } from "../../redux/actions/tmdbActions.js";
 
-import {testAction,searchMovie} from "../../redux/actions/tmdbActions.js";
-
-const key = "0204271af4a18c9dbc4c32fc858eff07";
-const tmdbUrl = `https://api.themoviedb.org/3/search/movie?api_key=${key}`;
-//https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&query=the%20cleansing%20hour&page=1&include_adult=false
+import { useDispatch } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
   search: {
@@ -37,22 +35,38 @@ const useStyles = makeStyles((theme) => ({
 
 const SearchBar = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const [query, setQuery] = useState("");
 
   const handleChange = (event) => {
     //console.log({ [event.target.name]: event.target.value });
     setQuery(event.target.value.toString());
-    searchMovie(query);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    //console.log(query);
+    // Goes to tmdbReducer after
+    dispatch(searchMovieByTitle(query));
   };
 
   return (
-    <Paper component="form" className={classes.search} width={6} onChange={handleChange}>
+    <Paper
+      component="form"
+      className={classes.search}
+      width={6}
+      onChange={handleChange}
+    >
       <InputBase
         className={classes.input}
         placeholder="Search TMDB"
         inputProps={{ "aria-label": "search tmdb" }}
       />
-      <IconButton className={classes.iconButton} aria-label="search">
+      <IconButton
+        className={classes.iconButton}
+        aria-label="search"
+        onClick={handleClick}
+      >
         <SearchIcon />
       </IconButton>
     </Paper>
