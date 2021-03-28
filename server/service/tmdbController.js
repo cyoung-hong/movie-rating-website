6b0df08b09eb3d5514e31adc8cdd49abe342b106
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-03-28 18:19:23 -0400

    fixed searchMovie function on the controller side

--- server/service/tmdbController.js
+++ server/service/tmdbController.js
@@ -12,16 +12,13 @@ export const testTMDB = (req, res) => {
 
 export const searchMovie = async (req, res) => {
   const { query } = req.params;
+  //console.log('Controller now in searchMovie');
   try {
-    await axios
-      .get(
-        `${process.env.TMDB_API}/search/movie?api_key=${process.env.TMDB_KEY}&language=en-US&query=${query}&page=1&include_adult=false&region=us`
-      )
-      .then((res) => {
-        console.log(res.data.results[0]);
-        res.status(200).json(res.data.results);
+    await axios.get(`${process.env.TMDB_API}/search/movie?api_key=${process.env.TMDB_KEY}&language=en-US&query=${query}&page=1&include_adult=false&region=us`)
+      .then((searchResults) => {
+        res.status(201).json(searchResults.data);
       })
-      .catch((err) => res.status(500).json(err));
+      .catch((err) => res.status(500).json(err.message));
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
