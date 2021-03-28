2af075fd411b88eb172c803345696ca53ba2f07d
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-03-28 18:17:01 -0400

    added action to search movies by title

--- client/src/redux/actions/tmdbActions.js
+++ client/src/redux/actions/tmdbActions.js
@@ -1,20 +1,21 @@
-import { SEARCH_MOVIE } from "../actionTypes.js";
+import { SEARCH_TMDB_TITLE } from "../actionTypes.js";
+import * as api from '../../api/index.js';
 import axios from "axios";
 
 export const testAction = (query) => {
     console.log("In test action " + query);
 }
 
-export const searchMovie = (movieTitle) => (dispatch) => {
+export const searchMovieByTitle = (movieTitle) => async (dispatch) => {
   //dispatch({ type: SEARCH_MOVIE });
   console.log("Firing searchMovie action ===================================")
-//   axios
-//     .get(`/search/${movieTitle}`)
-//     .then((res) => {
-//       dispatch({
-//         type: SEARCH_MOVIE,
-//         payload: res.data,
-//       });
-//     })
-//     .catch((err) => console.log(err));
+  try {
+    console.log('TRY HARDER');
+    const { data } = await api.queryTMDBTitle(movieTitle);
+
+    // payload should be res.data.results
+    dispatch({ type: SEARCH_TMDB_TITLE, payload: data});
+  } catch (err) {
+    console.log(`YIKES ${err}`);
+  }
 };
