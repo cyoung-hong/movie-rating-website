f43f1ecb3c62729390df3a3b4b1c13a680ae3dd0
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-04-05 18:12:22 -0400

    attempting error handling

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -1,24 +1,26 @@
-import { AUTH, SEARCH_TMDB_TITLE } from '../actionTypes.js';
+import { AUTH, SEARCH_TMDB_TITLE } from "../actionTypes.js";
 import * as api from "../../api/index.js";
 
-export const signin = (formData, history) => async(dispatch) => {
-    try {
+export const signin = (formData, history) => async (dispatch) => {
+  try {
+    const { data } = await api.signIn(formData);
+    dispatch({ type: AUTH, data });
+    //log in user
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-        //log in user
-        history.push('/');
-    } catch(error) {
-        console.log(error);
-    }
-}
+export const signup = (formData, history) => async (dispatch) => {
+  try {
+    // data = ({result: {} , token}) where result = new user
+    const { data } = await api.signUp(formData);
 
-export const signup = (formData, history) => async(dispatch) => {
-    try {
-        // data = ({result: {} , token}) where result = new user
-        const {data} = await api.signUp(formData);
-
-        dispatch({type: AUTH, payload: data});
-    } catch (err) {
-        console.log("authActions caught error");
-        console.log("Message?: " + err.message.message);
-    }
-}
+    dispatch({ type: AUTH, data });
+  } catch (error) {
+    console.log(error.response.data.message);
+    //dispatch({type: 'SET_ERROR', errorObj});
+    //console.log(error.message);
+  }
+};
