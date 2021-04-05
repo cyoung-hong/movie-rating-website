21f3aa54fa77047384da5ad2d5b3c5c17f653a45
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-04-05 18:15:51 -0400

    added auth middleware

--- server/routes/recommendationRoutes.js
+++ server/routes/recommendationRoutes.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import { getRecommendations, createRecommendation } from '../controllers/recommendationController.js';
 
+import auth from '../middleware/auth.js';
+
 const router = express.Router();
 
 router.get('/', getRecommendations);
-router.post('/create', createRecommendation);
+router.post('/create', auth, createRecommendation);
 
 
 export default router;
