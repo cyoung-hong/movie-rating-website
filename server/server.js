d319938b33efb82884a8741a76469adade8c4902
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-04-05 18:13:28 -0400

    beginning to implement passport

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // "This product uses the TMDb API but is not endorsed or certified by TMDb."
+// LOOK INTO EXPRESS VALIDATOR
 
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -10,6 +10,10 @@ import tmdbRoutes from './service/tmdb.js'
 import ratingRoutes from './routes/ratingRoutes.js';
 import userRoutes from './routes/userRoutes.js'
 import recommendationRoutes from './routes/recommendationRoutes.js';
+
+var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session'), bodyParser = require('body-parser');
+
 
 dotenv.config();
 
@@ -20,10 +24,23 @@ app.use(express.json({ limit: "30mb", extended: true}));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+
+
 app.use('/api/tmdb', tmdbRoutes);
 app.use('/api/recommendations', recommendationRoutes);
 app.use('/api/ratings', ratingRoutes);
 app.use('/api/user', userRoutes);
+
+app.use(async (req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error,req,res,next) => {
+    res.status(error.status);
+    res.json({message: error.message});
+});
 
 const PORT = process.env.PORT || 8082;
 
@@ -36,6 +53,5 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
-app.get('/', (req, res) => res.send('Hello world!'));
 
 
