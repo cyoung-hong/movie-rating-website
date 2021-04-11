3ad5686516681f899679b6f9814bb851f71f8d3c
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-04-10 23:32:52 -0400

    eh

--- server/middleware/Passport/auth.js
+++ server/middleware/Passport/auth.js
@@ -1,12 +1,18 @@
+// TODO
+// Might not need this honestly
 const auth = (req, res, next) => {
-    //console.log(req.session);
-    if(req.isAuthenticated()) {
-        console.log('Pass');
-        //console.log(res);
-        next(res.passport);
-    } else {
-        res.status(401).json({msg: 'Unauthorized access'});
-    }
-}
+  //console.log(req.session);
+  console.log('Middleware');
+  console.log(req.sessionID);
+  console.log(req.user);
+  if (req.isAuthenticated()) {
+    console.log("Pass");
+    //console.log(res);
+    next();
+  } else {
+    console.log('Fail');
+    res.status(401).json({ msg: "Unauthorized access" });
+  }
+};
 
 export default auth;
