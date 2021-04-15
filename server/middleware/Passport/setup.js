c2c3abf9047717bd086be1ab6c6c0ee18a1bbeee
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-04-14 22:44:17 -0400

    refactor

--- server/middleware/Passport/setup.js
+++ server/middleware/Passport/setup.js
@@ -1,6 +1,6 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
-import User from "../../models/user.js";
+import User from "../../models/User.js";
 import bcrypt from "bcryptjs";
 
 // ===================== Strategy =================== //
