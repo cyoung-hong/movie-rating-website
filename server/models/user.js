ca563227cdcb6c517d3adc61f19262aac5300f61
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-04-19 17:23:28 -0400

    added activeGroup

--- server/models/user.js
+++ server/models/user.js
@@ -7,6 +7,7 @@ const userSchema = mongoose.Schema({
     email: {type: String, required: true},
     password: {type: String, required: true},
     picturePath: String,
+    activeGroup: String,
     groups: [{id: String}]
 })
 
