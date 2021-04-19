be31096040dffe99f29328a21381a2da196a47e1
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-04-19 17:21:10 -0400

    deleted

--- server/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-export default errorHandler => (err, req, res, next) => {
-    res.send({
-        error: {
-            status: err.status || 500,
-            message: err.message,
-        }
-    })
-};
