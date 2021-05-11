220d09afb75bddb751a88754fe529e8729781b25
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-05-11 16:49:40 -0400

    setup for later

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,2 +1,11 @@
 import User from "../models/User.js";
+import GroupInquiry from "../models/GroupInquiry.js";
 
+export const getGroupInvites = async (req, res) => {
+  if (req.isAuthenticated()) {
+    const invites = await GroupInquiry.find({
+      origin: "group",
+      "user.id": req.user._id,
+    });
+  }
+};
