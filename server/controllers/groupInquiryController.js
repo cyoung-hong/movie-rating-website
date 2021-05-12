bee649486b332d75adc50ccfd0106fe6c1403fdf
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-05-12 16:17:27 -0400

    refactor

--- server/controllers/groupInquiryController.js
+++ server/controllers/groupInquiryController.js
@@ -3,38 +3,42 @@ import GroupInquiry from "../models/GroupInquiry.js";
 export const createInquiry = async (req, res) => {
   try {
     const { group, user } = req.body;
+    const foundInquiry = await GroupInquiry.findOne({
+      "group.id": group.id,
+      "user.id": user.id,
+    });
 
-    if (req.isAuthenticated()) {
-      const foundInquiry = await GroupInquiry.findOne({
-        "group.id": group.id,
-        "user.id": user.id,
-      });
+    if (!req.isAuthenticated()) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized access. Please login." });
+    }
 
-      if (foundInquiry) {
-        res.status(409).json({ error: "Request already exists" });
-      } else {
-        const newInquiry = new GroupInquiry({
-          group,
-          user,
-        });
+    if (foundInquiry) {
+        return res.status(409).json({ error: "Request already exists" });
+    } 
+    
+    const newInquiry = new GroupInquiry({
+      group,
+      user,
+    });
 
-        const savedInquiry = await newInquiry.save();
+    const savedInquiry = await newInquiry.save();
 
-        if (savedInquiry) {
-          return res.status(201).json({ savedInquiry });
-        }
+    if (savedInquiry) {
+      return res.status(201).json({ savedInquiry });
+    }
 
-        if (!savedInquiry) {
-          return res.status(500).json({ message: "Unable to save request." });
-        }
-      }
-    } else {
-      //Add redirect to login
-      console.log("Redirecting...");
-      res.status(401).json({ error: "Unauthorized, please sign in." });
+    if (!savedInquiry) {
+      return res.status(500).json({ message: "Unable to save request." });
     }
   } catch (error) {
     console.log(error);
     res.status(500).json({ error });
   }
 };
+
+export const deleteInquiryById = async(req,res) => {
+  
+}
+
