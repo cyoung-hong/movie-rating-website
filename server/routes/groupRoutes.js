eb0b582ae7d8b333ac83800262ff08cf682c2893
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-05-19 14:47:15 -0400

    prepared routes

--- server/routes/groupRoutes.js
+++ server/routes/groupRoutes.js
@@ -1,40 +1,43 @@
 import express from "express";
-import passport from "../middleware/Passport/setup.js";
 
 import {
+  getGroups,
   getGroupById,
   createGroup,
   addUser,
   changeUserRole,
+  removeUser,
+  deleteGroup,
 } from "../controllers/groupController.js";
-import Group from "../models/Group.js";
 
 const router = express.Router();
 
-router.get("/test/:id", getGroupById);
+router.get("/", getGroups);
+router.get("/group/:id", getGroupById);
 router.post("/create", createGroup);
 router.post("/add", addUser);
 router.post("/change", changeUserRole);
+router.post("/remove", removeUser);
+router.delete("/delete/:id", deleteGroup);
 
-import { body } from "express-validator";
-import User from "../models/User.js";
-import { validateRequest } from "../middleware/validateRequest.js";
-
-router.post(
-  "/test",
-  passport.authenticate("local-login"),
-  body().custom((value) => {
-    const { group, user } = value;
-    console.log(value);
-    console.log(group);
-    console.log(user);
-  }),
-  validateRequest,
-  passport.authenticate("local-login"),
-  async (req, res) => {
-    const { group } = req.body;
-    res.send(group);
-  }
-);
+// Potential validation middleware
+// import { body } from "express-validator";
+// import { validateRequest } from "../middleware/validateRequest.js";
+// router.post(
+//   "/test",
+//   passport.authenticate("local-login"),
+//   body().custom((value) => {
+//     const { group, user } = value;
+//     console.log(value);
+//     console.log(group);
+//     console.log(user);
+//   }),
+//   validateRequest,
+//   passport.authenticate("local-login"),
+//   async (req, res) => {
+//     const { group } = req.body;
+//     res.send(group);
+//   }
+// );
 
 export default router;
