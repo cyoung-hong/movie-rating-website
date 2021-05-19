d8cd4bd715689c94a0a16280f864bb3c1d622396
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-05-19 14:45:40 -0400

    redid layouts

--- client/src/components/ResultList/ResultDetail/ResultDetail.js
+++ client/src/components/ResultList/ResultDetail/ResultDetail.js
@@ -1,42 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Grid, Button, Typography } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 import {
-  Grid,
-  Button,
-  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  Modal,
+  Paper,
+  Box,
+  CardActionArea,
 } from "@material-ui/core";
-import { useDispatch } from 'react-redux';
-import { Card, CardContent, CardMedia } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { createRequest } from '../../../redux/actions/requestActions.js';
+import { createRequest } from "../../../redux/actions/requestActions.js";
+import moment from "moment";
+
+import MovieDialog from "../../MovieDialog/MovieDialog.js";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-    margin: 4,
-    width: "100%",
     height: 141,
+  },
+  cardHeader: {
+    justifyContent: "space-between",
+    alignContent: "center",
   },
   details: {
     display: "flex",
     flexDirection: "column",
-    alignContent: "center",
+  },
+  image: {
+    display: "flex",
+    minWidth: 94,
+    width: 95,
+    height: 141,
   },
   content: {
-    width: "100%",
-    height: "100%"
+    height: "100%",
+    alignContent: "space-between",
   },
-  cover: {
-    display: "flex",
-    width: 94,
+  overviewWrapper: {
+    display: "-webkit-box",
+    "-webkit-line-clamp": 2,
+    "-webkit-box-orient": "vertical",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  },
+  clickable: {
+    cursor: "pointer",
+    width: "100%",
   },
 }));
 
-const ResultDetail = ({result}) => {
+const ResultDetail = ({ result }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
 
   const handleClick = (event) => {
-    console.log('Step 1: Creating movieData object');
     const movieData = {
       movie: {
         tmdbID: result.id,
@@ -44,35 +65,142 @@ const ResultDetail = ({result}) => {
         year: result.release_date,
         posterUrl: result.poster_path,
         //genres: result.genres,
-      }
+      },
     };
-    console.log('Step 2: Dispatch object');
-    console.log(movieData);
+
     dispatch(createRequest(movieData));
-  }
-  
+  };
+
+  // TO DO
+  // Open paper with movie details
+  // Modal currently fires both actions
+  const openMovieModal = () => {
+    console.log("In open" + open);
+    setOpen(true);
+
+    //dispatch(getMovieDetails(movieData))
+  };
+
+  const handleClose = () => {
+    console.log("In Close" + open);
+    setOpen(false);
+  };
+
   return (
     <Card className={classes.root}>
       <CardMedia
-        className={classes.cover}
+        className={classes.image}
         image={`https://image.tmdb.org/t/p/w300${result.poster_path}`}
         title="poster"
       />
-      <Grid container className={classes.details}>
-        <CardContent className={classes.content}>
-          <Typography component="h5" variant="h5">
-            {result.original_title}
-          </Typography>
-          <Typography variant="subtitle1" color="textSecondary">
-            {result.overview}
-          </Typography>
-          <Button onClick={handleClick}> 
-            Adddd
-          </Button>
-        </CardContent>
-      </Grid>
+
+      <CardContent className={classes.clickable} onClick={openMovieModal}>
+        <Grid className={classes.content} container>
+          <Grid item xs={12}>
+            <Grid className={classes.cardHeader} container xs={12}>
+              <Grid item>
+                <Grid item className={classes.title}>
+                  {result.title}
+                </Grid>
+                <Grid item>
+                  <Typography variant="body1">
+                    {moment(result.year).format("MMMM D, YYYY")}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Button onClick={openMovieModal} variant="outlined">
+                  More
+                </Button>
+                <Button onClick={handleClick} variant="outlined">
+                  Add
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item>
+            <Grid container className={classes.overviewWrapper}>
+              <Typography variant="body1" color="textPrimary">
+                {result.overview}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </CardContent>
+      <MovieDialog open={open} handleClose={handleClose} movieData={result} />
     </Card>
   );
 };
 
 export default ResultDetail;
+
+{
+  /*     <Card className={classes.root}>
+      <CardMedia
+        className={classes.image}
+        image={`https://image.tmdb.org/t/p/w300${result.poster_path}`}
+        title="poster"
+      />
+      <CardContent onClick={() => {alert("Hi")}}>
+        <Grid className={classes.content} container>
+          <Grid item xs={12} >
+            <Grid className={classes.cardHeader} container xs={12}>
+              <Grid item>
+                <Grid item className={classes.title}>
+                  {result.title}
+                </Grid>
+                <Grid item>
+                  <Typography variant="body1">
+                    {moment(result.year).format("MMMM D, YYYY")}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Button onClick={handleClick} variant="outlined">Add</Button>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item>
+            <Grid container className={classes.overviewWrapper}>
+              <Typography variant="body1" color="textPrimary">
+                {result.overview}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>*/
+}
+
+{
+  /* <Card className={classes.root}>
+<CardActionArea>
+  <CardMedia
+    className={classes.image}
+    image={`https://image.tmdb.org/t/p/w300${result.poster_path}`}
+    title="poster"
+  />
+  <CardContent
+    onClick={() => {
+      alert("Hi");
+    }}
+  >
+    <Typography> {result.title} </Typography>
+
+    <Typography variant="body1">
+      {moment(result.year).format("MMMM D, YYYY")}
+    </Typography>
+
+    <Button onClick={handleClick} variant="outlined">
+      Add
+    </Button>
+
+    <Typography variant="body1" color="textPrimary">
+      {result.overview}
+    </Typography>
+  </CardContent>
+</CardActionArea>
+</Card> */
+}
--- client/src/components/ResultList/ResultList.js
+++ client/src/components/ResultList/ResultList.js
@@ -7,12 +7,51 @@ import { Grid, CircularProgress, CssBaseline } from "@material-ui/core";
 import ResultDetail from "./ResultDetail/ResultDetail.js";
 
 const useStyles = makeStyles((theme) => ({
-  resultContainer: {
+  columnWrapper: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    minHeight: "100vh",
+    padding: theme.spacing(8),
+    justifyContent: "center",
+    background:
+      "radial-gradient(circle, rgba(124,247,251,1) 0%, rgba(9,9,121,1) 100%)",
+  },
+  searchDetails: {
     position: "relative",
-    top: 14,
+    background: "#ffffff80",
+  },
+  resultContainer: {
     justifyContent: "center",
   },
 }));
+
+const array = [
+  {
+    title: "Movies",
+    count: 12,
+  },
+  {
+    title: "TV Shows",
+    count: 4,
+  },
+  {
+    title: "Companies",
+    count: 5,
+  },
+  {
+    title: "Keywords",
+    count: 45,
+  },
+  {
+    title: "Collections",
+    count: 45,
+  },
+  {
+    title: "Networks",
+    count: 0,
+  },
+];
 
 const ResultList = () => {
   const resultList = useSelector((state) => state.tmdbReducer.results);
@@ -26,12 +65,16 @@ const ResultList = () => {
   ) : (
     <React.Fragment>
       <CssBaseline />
-      <Grid container className={classes.resultContainer}>
-        {resultList.map((result) => (
-          <Grid key={result.id} item xs={8}>
-            <ResultDetail result={result} />
+      <Grid container className={classes.columnWrapper} xs={12} spacing={2}>
+        <Grid item xs={10}>
+          <Grid container className={classes.resultContainer} spacing={1}>
+            {resultList.map((result) => (
+              <Grid item key={result.id} xs={12}>
+                <ResultDetail result={result} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
+        </Grid>
       </Grid>
     </React.Fragment>
   );
