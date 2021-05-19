bb078295a95e9b68f05b11765305255b60b7ccc3
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-05-19 14:42:09 -0400

    removed main title

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -16,18 +16,12 @@ const Home = () => {
       <Container className={classes.theatre} disableGutters maxWidth={false}>
         <Grid className={classes.overlay} />
         <Grid container className={classes.content}>
-          <Grid item className={classes.textContent}>
-            <Typography variant="h1" className={classes.ugh}>
-              Agtown Movies
-            </Typography>
-          </Grid>
           <Grid item>
             <SearchBar />
           </Grid>
         </Grid>
       </Container>
       <Footer />
-
     </React.Fragment>
   );
 };
