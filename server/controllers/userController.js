7b1ec1919c049bfc0a02d99a1facd709673407aa
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-05-19 14:46:33 -0400

    preparing user endpoints

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,27 @@
 import User from "../models/User.js";
 import GroupInquiry from "../models/GroupInquiry.js";
 
+export const getUsers = async (req, res) => {
+  try {
+  const users = await User.find();
+  } catch(error) {
+    console.log(error);
+    res.status(500).json({message: "Internal server error."});
+  }
+}
+
+export const getUserById = async (req, res) => {
+
+}
+
+export const joinGroup = async (req, res) => {
+
+}
+
+export const leaveGroup = async (req, res) => {
+
+}
+
 export const getGroupInvites = async (req, res) => {
   if (req.isAuthenticated()) {
     const invites = await GroupInquiry.find({
