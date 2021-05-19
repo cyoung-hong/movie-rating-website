3e3b718eee8c7c9cd67d2626ed865e1470d3006b
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-05-19 14:47:43 -0400

    added routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,3 +2,15 @@
 // MAKE A DASHBOARD
 // NEED TO LIST ALL OF THE USERS RECOMMENDATIONS
 // ALSO CONSIDER ERROR HANDLER
+
+import express from "express";
+
+import {getUsers, getUserById, joinGroup, leaveGroup, deleteUser} from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.get("/", getUsers);
+router.get("/user/:id", getUserById);
+router.post("/user/group/join", joinGroup);
+router.post("/user/group/leave", leaveGroup);
+router.delete("user/delete", deleteUser);
