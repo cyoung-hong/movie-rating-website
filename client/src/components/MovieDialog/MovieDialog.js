96e41d94949f65b3ab337ad2d6b972668452fa31
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-05-19 21:27:38 -0400

    finished for now, added breakpoint for later

--- client/src/components/MovieDialog/MovieDialog.js
+++ client/src/components/MovieDialog/MovieDialog.js
@@ -1,33 +1,66 @@
 import React from "react";
-import { Dialog, Grid, Typography, useMediaQuery } from "@material-ui/core";
+import { Dialog, Grid, Typography, useMediaQuery, Button } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { createRequest } from "../../redux/actions/requestActions.js";
 import moment from "moment";
 
 import useStyles from "./styles";
 
 const MovieModal = (props) => {
-  const { open, handleClose, movieData } = props;
-  const posterPath = `https://image.tmdb.org/t/p/w500${movieData.poster_path}`;
+  const { open, handleClose, movie } = props;
+  const posterPath = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
   const classes = useStyles({ posterPath });
+  const dispatch = useDispatch();
 
-  const midget = useMediaQuery('min-width: 830px');
-  console.log(midget);
+  // Breakpoint for dialog directly
+  const bigboi = useMediaQuery("(min-width:680px)");
+  console.log(`is bigboi?${bigboi}`);
 
-  console.log(props);
+  const handleClick = (event) => {
+    const movieData = {
+      movie: {
+        tmdbID: movie.id,
+        title: movie.original_title,
+        year: movie.release_date,
+        posterUrl: movie.poster_path,
+        //genres: result.genres,
+      },
+    };
+  
+    dispatch(createRequest(movieData));
+  };
+
   return (
     <Dialog className={classes.dialog} open={open} onClose={handleClose}>
       <Grid container>
         <Grid className={classes.poster} item xs={4} />
 
-        <Grid item className={classes.wrapper} xs={8}>
-          <Grid container className={classes.info}> 
-            <Grid item className={classes.infoDetails}>
-              <Typography variant="h6">{movieData.title}</Typography>
+        <Grid container className={classes.wrapper} xs={8}>
+          <Grid item>
+            <Grid container className={classes.info}>
+              <Grid item className={classes.infoDetails}>
+                <Typography variant="h6">{movie.title}</Typography>
+              </Grid>
+              <Grid item className={classes.infoDetails}>
+                <Typography>
+                  {moment(MovieModal.release_date).format("MMMM D, YYYY")}
+                </Typography>
+              </Grid>
+              <Grid item className={classes.infoDetails}>
+                <Typography variant="body1">{movie.overview}</Typography>
+              </Grid>
             </Grid>
-            <Grid item className={classes.infoDetails}>
-              <Typography>  {moment(movieData.release_date).format("MMMM D, YYYY")}</Typography>
-            </Grid>
-            <Grid item className={classes.infoDetails}>
-              <Typography variant="body1">{movieData.overview}</Typography>
+          </Grid>
+
+          <Grid item className={classes.buttonWrapper}>
+            <Grid container className={classes.buttonContainer}>
+              <Button
+                className={classes.button}
+                onClick={handleClick}
+                variant="outlined"
+              >
+                Add
+              </Button>
             </Grid>
           </Grid>
         </Grid>
