d845b3234fe56cd97eef6ee4639f90e9a24fef3d
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-05-25 21:24:11 -0400

    added reducers

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 
 import ratingReducer from './ratingReducer.js';
+import recReducer from './recReducer.js';
 import authReducer from './authReducer.js';
 import tmdbReducer from './tmdbReducer.js';
 
 export default combineReducers({
     ratingReducer,
-    authReducer,
     tmdbReducer,
+    recReducer,
+    authReducer,
 });
