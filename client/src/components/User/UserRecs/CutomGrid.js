bbbf558ebfb408e5ced52341c206232fdac7c428
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-06-03 23:03:30 -0400

    setting up additional rec lists

--- client/src/components/User/UserRecs/CutomGrid.js
+++ client/src/components/User/UserRecs/CutomGrid.js
@@ -1,37 +1,39 @@
 import React from "react";
-import { Grid} from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import CustomTile from "./CustomTile.js";
 import { useSelector } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
-  tileWrapper: {
-
-  },
+  tileWrapper: {},
   gridContainer: {
     flexGrow: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   tile: {
     height: 200,
     width: 100,
-  }
+  },
 }));
 
-
-const CustomGrid = () => {
+const CustomGrid = ({ list }) => {
   const classes = useStyles();
-  const recList = useSelector((state) => state.recReducer.myRecs);
 
   return (
     <Grid className={classes.gridWrapper} item xs={12}>
       <Grid className={classes.gridContainer} spacing={1} container>
-        {recList.map((rec) => (
+        {list ? (
+          list.map((rec) => (
+            <Grid className={classes.tileWrapper} item>
+              <CustomTile className={classes.tile} rec={rec} />
+            </Grid>
+          ))
+        ) : (
           <Grid className={classes.tileWrapper} item>
-            <CustomTile className={classes.tile} rec={rec} />
+            <Typography> No items in list.</Typography>
           </Grid>
-        ))}
+        )}
       </Grid>
     </Grid>
   );
