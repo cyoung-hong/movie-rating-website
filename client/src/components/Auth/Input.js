c523dfe07ab9fac12a20851534b04a74913ba01d
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-06-07 22:05:20 -0400

    added props to input

--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -1,34 +1,50 @@
-import React from 'react';
-import { TextField, Grid, InputAdornment, IconButton} from '@material-ui/core';
-import  Visibility  from '@material-ui/icons/Visibility';
-import  VisibilityOff  from '@material-ui/icons/VisibilityOff';
+import React from "react";
+import { TextField, Grid, InputAdornment, IconButton } from "@material-ui/core";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
 // INPUT PROPS GENERATES A WARNING
 
-const Input = ({name, handleChange, label, autoFocus, type, handleShowPassword, half}) => {
-    return (
-        <Grid item xs={12} sm={half ? 6 :12}>
-            <TextField
-                name={name}
-                onChange={handleChange}
-                variant="outlined"
-                required
-                fullWidth
-                label={label}
-                autoFocus={autoFocus}
-                type={type}
-                InputProps={name === 'password' ? {
-                    endAdornment: (
-                        <InputAdornment position="end">
-                            <IconButton onClick={handleShowPassword}>
-                                {type === 'password' ? <Visibility /> : <VisibilityOff />}
-                            </IconButton>
-                        </InputAdornment>
-                    ),
-                } : null} 
-            />
-        </Grid>
-    )
-}
+const Input = ({
+  name,
+  handleChange,
+  label,
+  autoFocus,
+  type,
+  handleShowPassword,
+  half,
+  errorExists,
+  helperText
+}) => {
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+      <TextField
+        name={name}
+        onChange={handleChange}
+        variant="outlined"
+        required
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        error={errorExists}
+        helperText={helperText}
+        InputProps={
+          name === "password"
+            ? {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleShowPassword}>
+                      {type === "password" ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }
+            : null
+        }
+      />
+    </Grid>
+  );
+};
 
 export default Input;
