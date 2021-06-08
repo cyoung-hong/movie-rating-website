bf54cfda3fc1ec4d93b78a83fde53322fde1e972
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-06-07 22:04:22 -0400

    added timer for inactivity and reactivity

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
@@ -8,13 +9,18 @@ import themeFile from "./theme.js";
 import Navbar from "./components/Navbar/Navbar.js";
 
 //Redux
-//import { useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 
+// Components
 import Home from "./components/Home/Home.js";
 import Auth from "./components/Auth/Auth.js";
 import ResultList from "./components/ResultList/ResultList.js";
 import User from "./components/User/User.js";
-import GroupForm from './components/Group/CreateGroup.js';
+import GroupForm from "./components/Group/CreateGroup.js";
+
+import { useIdleTimer } from "react-idle-timer";
+
+import {authenticate} from './redux/actions/authActions.js';
 
 axios.defaults.baseURL = "https:/localhost:8082/api/";
 axios.defaults.withCredentials = true;
@@ -22,6 +28,22 @@ axios.defaults.withCredentials = true;
 const theme = createMuiTheme(themeFile);
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const handleOnIdle = (event) => {
+    dispatch(authenticate());
+  };
+
+  const handleOnActive = (event) => {
+    dispatch(authenticate());
+  };
+
+  const { getRemainingTime, getLastActiveTime } = useIdleTimer({
+    timeout: 1000 * 60 * 2,
+    onIdle: handleOnIdle,
+    onActive: handleOnActive,
+  });
+
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
@@ -30,7 +52,7 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/auth" component={Auth} />
           <Route exact path="/results" component={ResultList} />
-          <Route exact path="/user" component={User}/>
+          <Route exact path="/user" component={User} />
           <Route exact path="/group" component={GroupForm} />
         </Switch>
       </BrowserRouter>
