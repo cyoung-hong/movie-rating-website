d7beebc7dde7251cc19eced0dfc07cc0ace91535
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-06-08 21:34:01 -0400

    refactor

--- client/src/components/Form/TextfieldWrapper.js
+++ client/src/components/Form/TextfieldWrapper.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import {TextField} from '@material-ui/core';
-import {useField} from 'formik'
+import React from "react";
+import { TextField } from "@material-ui/core";
+import { useField } from "formik";
 
-const Textfield = ({name, ...otherProps}) => {
-    // meta contains touched and error state
-    const [field, meta] = useField(name);
+const Textfield = ({ name, ...otherProps }) => {
+  // meta contains touched and error state
+  const [field, meta] = useField(name);
 
-    const configTextField = {
-        ...field,
-        ...otherProps,
-        fullWidth: true,
-        variant: 'outlined',
-    };
 
-    if(meta && meta.touched && meta.error) {
-        configTextField.error = true;
-        configTextField.helperText = meta.error;
-    }
+  const configTextField = {
+    ...field,
+    ...otherProps,
+    fullWidth: true,
+    variant: "outlined",
+  };
 
-    return (
-        <TextField {...configTextField} />
-    )
-}
+  if (meta && meta.touched && meta.error) {
+    configTextField.error = true;
+    configTextField.helperText = meta.error;
+  }
+
+  return (
+    <TextField
+      {...configTextField}
+    />
+  );
+};
 
 export default Textfield;
