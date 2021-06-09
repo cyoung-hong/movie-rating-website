15202377a6907068880e96f0452441a90163bfb8
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-06-08 21:33:38 -0400

    formik refactor

--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -1,50 +1,36 @@
 import React from "react";
 import { TextField, Grid, InputAdornment, IconButton } from "@material-ui/core";
+import { useField } from "formik";
+
 import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
 // INPUT PROPS GENERATES A WARNING
 
-const Input = ({
-  name,
-  handleChange,
-  label,
-  autoFocus,
-  type,
-  handleShowPassword,
-  half,
-  errorExists,
-  helperText
-}) => {
-  return (
-    <Grid item xs={12} sm={half ? 6 : 12}>
-      <TextField
-        name={name}
-        onChange={handleChange}
-        variant="outlined"
-        required
-        fullWidth
-        label={label}
-        autoFocus={autoFocus}
-        type={type}
-        error={errorExists}
-        helperText={helperText}
-        InputProps={
-          name === "password"
-            ? {
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={handleShowPassword}>
-                      {type === "password" ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }
-            : null
-        }
-      />
-    </Grid>
-  );
+const Input = ({ name, ...props }) => {
+  const [field, meta] = useField(name);
+  console.log(props);
+
+  const configTextField = {
+    ...field,
+    ...props,
+    // InputProps: name === "password" && {
+    //   endAdornment: (
+    //     <InputAdornment position="end">
+    //       <IconButton onClick={props.handleShowPassword}>
+    //         {props.type === "password" ? <Visibility /> : <VisibilityOff />}
+    //       </IconButton>
+    //     </InputAdornment>
+    //   ),
+    // },
+  };
+
+  if (meta && meta.touched && meta.error) {
+    configTextField.error = true;
+    configTextField.helperText = meta.error;
+  }
+
+  return <TextField {...configTextField} />;
 };
 
 export default Input;
