3c6764fa68a3667077314a848a82f6c0c5aa162c
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-06-25 21:26:05 -0400

    added ref to active group

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import Group from './Group.js';
 
 const userSchema = mongoose.Schema({
     username: {type: String, required: true},
@@ -7,8 +8,8 @@ const userSchema = mongoose.Schema({
     email: {type: String, required: true},
     password: {type: String, required: true},
     picturePath: {type: String, default: '../public/images/default.png'},
-    activeGroup: String,
-    groups: [{id: String}]
+    activeGroup: {type: mongoose.Schema.ObjectId, ref: 'Group', default: null},
+    groups: [{id: mongoose.Schema.ObjectId}]
 })
 
 const User = mongoose.model(`User`, userSchema);
