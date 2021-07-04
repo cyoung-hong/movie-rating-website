98c430c23a4c0b913729c28cd3394fa787f3c0d7
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-07-04 18:21:22 -0400

    added redirect from logout and temp link

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,29 +1,24 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { signOut } from "../../redux/actions/authActions.js";
 
 //MUI
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Grid,
-  Button,
-} from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Grid, Button } from "@material-ui/core";
 
 import useStyles from "./styles";
-
 
 const Navbar = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const history = useHistory();
 
-  const user = useSelector((state) => state.authReducer.user);
-  const loggedIn = useSelector((state) => state.authReducer.loggedIn);
+  const user = useSelector((state) => state.auth.user);
+  const loggedIn = useSelector((state) => state.auth.loggedIn);
 
   const handleClick = () => {
-   dispatch(signOut());
+    dispatch(signOut())
+    history.push("/");
   };
 
   return (
@@ -31,19 +26,46 @@ const Navbar = () => {
       <Toolbar className={classes.navbar}>
         <Grid container className={classes.menuContainer}>
           <Grid item>
-            <Typography component={Link} to={"/"} color="textSecondary" className={classes.homeLink}>AGTOWN MOVIES</Typography>
+            <Typography
+              component={Link}
+              to={"/"}
+              color="textSecondary"
+              className={classes.homeLink}
+            >
+              AGTOWN MOVIES
+            </Typography>
           </Grid>
 
-          {loggedIn? (
+          {loggedIn ? (
             <Grid
               container
               className={classes.subMenu}
               spacing={2}
               alignItems="center"
             >
-              <Typography component={Link} to={"/user"} className={classes.userName} variant="body1" color="textSecondary">
-                {user.username}
-              </Typography>
+              <Grid item>
+                <Typography
+                  component={Link}
+                  to={"/group"}
+                  variant="body1"
+                  color="textSecondary"
+                >
+                  Group
+                </Typography>
+              </Grid>
+
+              <Grid item>
+                <Typography
+                  component={Link}
+                  to={"/user"}
+                  className={classes.userName}
+                  variant="body1"
+                  color="textSecondary"
+                >
+                  {user.username}
+                </Typography>
+              </Grid>
+              
               <Grid item className={classes.searchWrapper}>
                 <Button
                   variant="contained"
