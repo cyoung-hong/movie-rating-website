235006f8c675301e48b626d33b132ce030034d83
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-07-04 18:24:43 -0400

    added case

--- client/src/redux/reducers/groupReducer.js
+++ client/src/redux/reducers/groupReducer.js
@@ -1,7 +1,18 @@
 import {CREATE_GROUP} from "../actionTypes.js";
 
-const initialData = {};
+const initialData = {
+    groupName: null,
+    groupPhoto: null,
+    members: [],
+    groupRecs: []
+};
 
 const groupReducer = (state = initialData, action) => {
-
+    switch(action.type){
+        case CREATE_GROUP: 
+        default:
+            return state;
+    }
 }
+
+export default groupReducer;
