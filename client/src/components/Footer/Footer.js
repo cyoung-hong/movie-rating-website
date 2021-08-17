e26c381cd5a24657042254a7e92a241477e8c937
Chris Young-Hong <cyoung-hong@myseneca.ca>
2021-08-17 17:04:25 -0400

    created footer component

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,32 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-import { Container, Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    padding: theme.spacing(0.5),
+    backgroundColor: "#000",
+    position: "fixed",
+    bottom: 0,
+    left: 0,
+    width: "100vw",
+  },
+  disclaimer: {
+    width: "100vw",
+
+  }
+}));
 
 const Footer = () => {
+  const classes = useStyles();
+
   return (
-    <Container position="relative">
-      <Typography>
+    <Grid className={classes.footer}>
+      <Typography className={classes.disclaimer} color="textSecondary" variant="h6">
         This product uses the TMDb API but is not endorsed or certified by TMDb.
       </Typography>
-    </Container>
+    </Grid>
   );
 };
 
